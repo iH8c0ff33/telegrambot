adc28af2e560158028a905209bfb8fe2f83e6944
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-01-24 11:22:22 +0100

    test

--- config/telegram.js
+++ config/telegram.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var token = fs.readFileSync(__dirname+'/../secure/botToken.key').toString('utf8');
+var token = fs.readFileSync(__dirname+'/../secure/botToken.key').toString('utf8').split('\n').join('');
 module.exports = {
   token: token,
   apiUrl: 'https://api.telegram.org/bot'+token+'/'
