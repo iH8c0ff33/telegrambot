de0cb87b6e360356bfb2106056305b02606b47d9
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-01-24 11:19:06 +0100

    added utf8

--- config/telegram.js
+++ config/telegram.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var token = fs.readFileSync(__dirname+'/../secure/botToken.key');
+var token = fs.readFileSync(__dirname+'/../secure/botToken.key').toString('utf8');
 module.exports = {
   token: token,
   apiUrl: 'https://api.telegram.org/bot'+token+'/'
